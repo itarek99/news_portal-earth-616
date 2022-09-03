ec80863ffbe0cd38c226bbfffdd0560ac724b6a6
itarek99 <tarekulislam7060@gmail.com>
2022-09-03 14:29:19 +0600

    static modal added

--- js/script.js
+++ js/script.js
@@ -12,8 +12,16 @@
 const renderCategory = (categories) => {
   const categoryContainer = document.getElementById('category');
   categories.forEach((category) => {
-    const buttonHtml = `<button onclick='categoryBtnClickHandler(this)' data-id=${category.category_id} class="btn-category border-0 bg-transparent py-1">${category.category_name}</button>`;
-    categoryContainer.insertAdjacentHTML('beforeend', buttonHtml);
+    console.log(category);
+    if (category.category_id == '01') {
+      const buttonHtml = `<button onclick='categoryBtnClickHandler(this)' data-id=${category.category_id} class="btn-category border-0 bg-transparent py-1 text-primary">${category.category_name}</button>`;
+      categoryContainer.insertAdjacentHTML('beforeend', buttonHtml);
+      console.log();
+      fetchNews(category.category_id, category.category_name);
+    } else {
+      const buttonHtml = `<button onclick='categoryBtnClickHandler(this)' data-id=${category.category_id} class="btn-category border-0 bg-transparent py-1">${category.category_name}</button>`;
+      categoryContainer.insertAdjacentHTML('beforeend', buttonHtml);
+    }
   });
 };
 
@@ -21,15 +29,16 @@ const categoryBtnClickHandler = (element) => {
   const categoryBtns = document.querySelectorAll('.btn-category');
   categoryBtns.forEach((categoryBtn) => {
     categoryBtn.classList.remove('text-primary');
+    categoryBtn.disabled = false;
   });
   element.classList.add('text-primary');
+  element.disabled = true;
   fetchNews(element.dataset.id, element.innerText);
 
   document.querySelector('.loading-animation').classList.remove('d-none');
 };
 
 const fetchNews = async (categoryId, categoryTitle) => {
-  console.log(categoryId, categoryTitle);
   try {
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
     const { data } = await response.json();
@@ -42,7 +51,6 @@ const fetchNews = async (categoryId, categoryTitle) => {
 const renderNews = (newsData, categoryTitle) => {
   document.getElementById('item-found').innerText = `${newsData.length} items found for category ${categoryTitle}`;
   const sortedNewsData = newsData.sort((a, b) => b.total_view - a.total_view);
-  console.log(sortedNewsData);
   const newsContainer = document.getElementById('news');
   newsContainer.innerHTML = '';
   sortedNewsData.forEach((data) => {
@@ -51,13 +59,39 @@ const renderNews = (newsData, categoryTitle) => {
   document.querySelector('.loading-animation').classList.add('d-none');
 };
 
+const renderModal = (singleNewsData) => {
+  console.log(singleNewsData);
+  const { title, image_url, details, total_view, others_info } = singleNewsData;
+  document.querySelector('.modal-title').innerText = title;
+  document.getElementById('modal-image').src = image_url;
+  document.getElementById('modal-text').innerText = details;
+  document.getElementById('total-view').innerText = !total_view ? 0 : total_view;
+  document.getElementById('trending').innerHTML = others_info.is_trending
+    ? `<i class="fa-solid fa-arrow-trend-up"></i>`
+    : `<i class="fa-solid fa-arrow-trend-down"></i>`;
+
+  console.log(others_info.is_trending);
+};
+
+const fetchSingleNews = async (news_id) => {
+  try {
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`);
+    const { data } = await response.json();
+    renderModal(data[0]);
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(news_id);
+};
+
 const newsCardHtml = (data) => {
-  const { author, details, thumbnail_url, title, total_view } = data;
+  const { author, details, thumbnail_url, title, total_view, _id } = data;
   const options = { year: 'numeric', month: 'short', day: 'numeric' };
   const published_date =
     author.published_date === null ? 'N/A' : new Date(author.published_date).toLocaleDateString(undefined, options);
 
-  return `<div class="card mb-5">
+  return `<div class="card mb-5 rounded-5">
 <div class="d-flex flex-column flex-md-row">
   <div class="flex-flex-shrink-0">
     <img class="card-thumb"
@@ -66,11 +100,11 @@ const newsCardHtml = (data) => {
   <div class="">
     <div class="card-body p-4">
       <div class="news-text">
-        <h5 class="card-title">${title}</h5>
+        <h5 class="card-title" role="button">${title}</h5>
         <p class="card-text">${details.length > 190 ? details.slice(0, 190) : details}...</p>
       </div>
-      <div class="d-flex align-items-center mt-3">
-        <div class="author d-flex align-items-center me-5">
+      <div class="d-flex flex-wrap align-items-center mt-3">
+        <div class="author d-flex align-items-center me-5 py-3">
           <div class="author-img me-2">
             <img class="img-fluid rounded-circle" src="${author.img}" alt="author">
           </div>
@@ -79,13 +113,13 @@ const newsCardHtml = (data) => {
             <p class="m-0 text-secondary fs-14">${published_date}</p>
           </div>
         </div>
-        <div class="view ms-2 ms-md-5">
+        <div class="view ms-0 ms-md-5">
           <div class="d-flex align-items-center">
             <i class="fa-solid fa-binoculars me-2"></i>
             <p class="fw-bold m-0">${total_view === null ? 0 : total_view}</p>
           </div>
         </div>
-        <button class="ms-auto border-0 bg-transparent text-primary">
+        <button onclick='fetchSingleNews("${_id}")' class="ms-auto border-0 bg-transparent text-primary" data-bs-toggle="modal" data-bs-target="#newsModal">
           <i class="fa-solid fa-chevron-right"></i>
         </button>
       </div>
